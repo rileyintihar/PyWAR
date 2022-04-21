210e8cbac8b75177aac4c082dd490f6e9afdd4ac
Riley Intihar <102986436+rileyintihar@users.noreply.github.com>
2022-04-20 20:11:10 -0400

    Delete newplayer.py
    Outdated name for player.py.

--- newplayer.py
+++
@@ -1,8 +0,0 @@
-# Adds new player to player database
-
-class Player:
-    def __init__(self, name, number, throw, bat):
-        self.name = name
-        self.number = number
-        self.throw = throw
-        self.bat = bat
